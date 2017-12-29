828dd58f17d8bf04feadb0d643443e1322672a22
gretzky <sfed617@gmail.com>
2017-12-29 10:02:37 -0500

    setup file updates

--- src/setup.sh
+++ src/setup.sh
@@ -145,11 +145,11 @@ main() {
 "
     sleep 1
 
-    cd "$(dirname "${BASH_SOURCE[0]}")/src" \
+    cd "$(dirname "${BASH_SOURCE[0]}")" \
         || exit 1
 
-    if [ -x "helpers.sh" ]; then
-        . "helpers.sh" || exit 1
+    if [ -x "src/init/helpers.sh" ]; then
+        . "src/init/helpers.sh" || exit 1
     else
         download_utils || exit 1
     fi
