5d430d32df59a24c0673bac8a716d33224951ecf
gretzky <sfed617@gmail.com>
2017-12-29 09:56:12 -0500

    setup file updates

--- src/setup.sh
+++ src/setup.sh
@@ -148,8 +148,8 @@ main() {
     cd "$(dirname "${BASH_SOURCE[0]}")" \
         || exit 1
 
-    if [ -x "./init/helpers.sh" ]; then
-        . "./init/helpers.sh" || exit 1
+    if [ -x "./src/init/helpers.sh" ]; then
+        . "./src/init/helpers.sh" || exit 1
     else
         download_utils || exit 1
     fi
