326735924878afa690683c0138f074f7687a9194
gretzky <sfed617@gmail.com>
2017-12-29 10:13:47 -0500

    fix homebrew

--- src/brew/setup.sh
+++ src/brew/setup.sh
@@ -11,29 +11,15 @@ install_homebrew() {
     print_result $? "Install Homebrew"
 }
 
-opt_out_of_analytics() {
-    local path=""
-
-    path="$(get_homebrew_git_config_file_path)" \
-        || return 1
-
-    if [ "$(git config --file="$path" --get homebrew.analyticsdisabled)" != "true" ]; then
-        git config --file="$path" --replace-all homebrew.analyticsdisabled true &> /dev/null
-    fi
-
-    print_result $? "Homebrew (opt-out of analytics)"
-}
-
 main() {
     print_header "🍺  Installing Homebrew..."
 
     install_homebrew
-    opt_out_of_analytics
 
     print_done
 
     print_header "🍻  Installing packages..."
-    ./brew.sh
+    ./packages.sh
 
     print_done
 }
